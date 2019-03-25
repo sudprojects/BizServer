fc671e44a568824c05c5234479b2b0ac73597958
Sudhanshu Shevade <sudhanshu.shevade@gmail.com>
2019-03-25 00:49:03 -0400

    Modified db settings

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const bcrypt = require('bcrypt-nodejs');
 const db  = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'bizdb'
+    connectionString : process.env.DATABASE_URL,
+	ssl: true,
   }
 });
 
